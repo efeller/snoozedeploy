e7f44bb499885c2a74fb24885ed1b8116f460fa2
Eugen Feller <eugen.feller@inria.fr>
2013-03-05 23:57:58 +0100

    Fix qemu utils

--- grid5000/deployscript/scripts/remote/install_bootstrap_dependencies.sh
+++ grid5000/deployscript/scripts/remote/install_bootstrap_dependencies.sh
@@ -2,4 +2,4 @@
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get -y install nfs-server openjdk-6-jre zookeeper zookeeperd nfs-common bridge-utils
+apt-get -y install nfs-server openjdk-6-jre qemu-utils zookeeper zookeeperd nfs-common bridge-utils
